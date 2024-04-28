a79cb8e5f1150a885f5c0b090c8403d33580d1ab
Julesntizimira <ntizimijules5@gmail.com>
2024-04-29 00:30:13 +0200

    update

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,12 +1,5 @@
-import $ from "jquery";
+import $ from 'jquery';
 
-
-$(function() {
-    const p1 = $('<p>').text('Holberton Dashboard');
-    const p2 = $('<p>').text('Dashboard data for the students');
-    const p3 = $('<p>').text('Copyright - Holberton School');
-
-    $('body').append(p1);
-    $('body').append(p2);
-    $('body').append(p3);
-})
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
