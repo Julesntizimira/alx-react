16e680b30201d9af04d2dc1d80bb3ced34671f50
Julesntizimira <ntizimijules5@gmail.com>
2024-04-29 08:22:38 +0200

    task one

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
     mode: 'production',
     entry: {
-        bundle: path.resolve(__dirname, "js/dashboard_main.js")
+        main: path.resolve(__dirname, "js/dashboard_main.js")
     },
     output: {
         path: path.resolve(__dirname, 'public'),
-        filename: '[name].js'
+        filename: 'bundle.js'
     }
 }
